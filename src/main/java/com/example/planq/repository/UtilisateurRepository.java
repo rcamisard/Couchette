9b813eefa442a244192d8ccb9b88b6fac7feeb3d
Romain Camisard <romain.camisard@centrale-marseille.fr>
2022-08-26 22:15:38 +0200

    RCD - Fix imports

--- src/main/java/com/example/planq/repository/UtilisateurRepository.java
+++ src/main/java/com/example/planq/repository/UtilisateurRepository.java
@@ -1,7 +1,6 @@
 package com.example.planq.repository;
 
 import com.example.planq.model.Utilisateur;
-import jdk.jshell.execution.Util;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
